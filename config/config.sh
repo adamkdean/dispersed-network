652eaaed146fbc2229de72ed0d1fb34b84161736
Adam K Dean <adamkdean@googlemail.com>
2017-11-04 15:32:30 +0000

    feat: construct QUEUE_ADDRESS in config

--- config/config.sh
+++ config/config.sh
@@ -14,7 +14,12 @@
 #
 # queue configuration
 #
-export QUEUE_HOSTNAME="gracious-gateway.akd.sh"
-export QUEUE_USERNAME="secretUser"
-export QUEUE_PASSWORD="tH1s15th3Secr3tp4ss"
-export QUEUE_VHOST="/dhttp"
+QUEUE_PROTOCOL="amqp" # amqps
+QUEUE_HOSTNAME="gracious-gateway.akd.sh"
+QUEUE_USERNAME="secretUser"
+QUEUE_PASSWORD="tH1s15th3Secr3tp4ss"
+QUEUE_VHOST="/dhttp"
+QUEUE_PARAMS="?heartbeat=1"
+
+# export the address which is all we need
+export QUEUE_ADDRESS="$QUEUE_PROTOCOL://$QUEUE_USERNAME:$QUEUE_PASSWORD@$QUEUE_HOSTNAME/$QUEUE_VHOST$QUEUE_PARAMS"
