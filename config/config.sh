41f83655b14ffa9c9eb8063fe8b8a3507da2c934
Adam K Dean <adamkdean@googlemail.com>
2017-11-04 22:46:20 +0000

    feat: add registry config

--- config/config.sh
+++ config/config.sh
@@ -20,6 +20,10 @@ QUEUE_USERNAME="secretUser"
 QUEUE_PASSWORD="tH1s15th3Secr3tp4ss"
 QUEUE_VHOST="/dhttp"
 QUEUE_PARAMS="?heartbeat=1"
-
-# export the address which is all we need
 export QUEUE_ADDRESS="$QUEUE_PROTOCOL://$QUEUE_USERNAME:$QUEUE_PASSWORD@$QUEUE_HOSTNAME/$QUEUE_VHOST$QUEUE_PARAMS"
+
+#
+# registry configuration
+#
+export REGISTRY_EMAIL="akd@dadi.co"
+export REGISTRY_DOMAIN="registry.akd.sh"
