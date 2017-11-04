fe6e03a6c0a1653c28e06eaf10ffe476bb340eb7
Adam K Dean <adamkdean@googlemail.com>
2017-11-04 23:38:02 +0000

    feat: add rest of registry bootstrap script

--- registry/bootstrap.sh
+++ registry/bootstrap.sh
@@ -38,4 +38,55 @@ if ! [ -x "$(command -v certbot)" ]; then
   echo "-----------------------------------"
 fi
 
-echo "..."
+
+read -p "Setup SSL certificate? [Y/N] " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  # generate SSL certificate for domain
+  certbot \
+    certonly \
+    --standalone \
+    --keep-until-expiring \
+    --email $REGISTRY_EMAIL \
+    -d $REGISTRY_DOMAIN
+
+  # setup letsencrypt certificates renewing
+  CRON_TEXT="30 2 * * 1 certbot renew >> /var/log/letsencrypt-renew.log"
+  CRON_EXISTS=$(cat /etc/crontab | grep "$CRON_TEXT")
+  if [ -z $CRON_EXISTS ]; then
+    echo $CRON_TEXT >> /etc/crontab
+  fi
+
+  # rename SSL certificates
+  SSL_DIR="/etc/letsencrypt/live/$REGISTRY_DOMAIN/"
+  cp $SSL_DIR/privkey.pem $SSL_DIR/domain.key
+  cat $SSL_DIR/cert.pem $SSL_DIR/chain.pem > $SSL_DIR/domain.crt
+  chmod 777 $SSL_DIR/domain.crt $SSL_DIR/domain.key
+  
+  echo "-----------------------------------"
+  echo "      SSL CERT SETUP COMPLETE      "
+  echo "-----------------------------------"
+fi
+
+# create registry volume if required
+QUEUE_VOLUME_EXISTS=$(docker volume ls --format "{{.Name}}" | grep ^dhttp-registry-data$)
+if [ -z $QUEUE_VOLUME_EXISTS ]; then
+  docker volume create \
+    dhttp-registry-data
+fi
+
+# run docker registry
+docker run \
+  --name registry \
+  --volume /etc/letsencrypt/live/$REGISTRY_DOMAIN:/certs \
+  --volume dhttp-registry-data:/var/lib/registry \
+  --env REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+  --env REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+  --publish 5000:5000 \
+  --restart=always \
+  --detatch \
+  registry:2
+
+echo "-----------------------------------"
+echo "       REGISTRY CONTAINER UP       "
+echo "-----------------------------------"
