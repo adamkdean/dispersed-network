056dee3f96c35d7d5934c04af0790d682dbf4cd1
Adam K Dean <adamkdean@googlemail.com>
2017-11-13 21:43:02 +0000

    feat: swap simple blog app for DADI blog

--- apps/blog/src/server.js
+++ apps/blog/src/server.js
@@ -1,15 +1,1 @@
-'use strict'
-
-const express = require('express')
-const app = express()
-const serverPort = process.env.PORT || 80
-
-app.use(express.static('public'))
-app.use((req, res) => {
-  console.log('request incoming:', req.url)
-  res.send('hello, this is a blog')
-})
-
-app.listen(serverPort, () => {
-  console.log(`blog listening on port ${serverPort}`)
-})
+require('@dadi/web')({"engines":[require("@dadi/web-dustjs")]})
