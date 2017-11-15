180d27116d9cebbdf79519ae29e6ad69e898ccdc
Adam K Dean <adamkdean@googlemail.com>
2017-11-15 00:09:06 +0000

    fix: set hello-world image to 600px

--- apps/hello-world/src/server.js
+++ apps/hello-world/src/server.js
@@ -10,7 +10,7 @@ app.use((req, res) => {
   console.log('req.headers', req.headers)
   const name = req.query && req.query.name || 'no-name'
   const head = `<head><title>Hello world</title><style>body { background: #e1e1e1; }.hello-img { margin-top: 100px; }</style></head>`
-  const body = `<body><center class="hello-img"><img src="/hello.png"></center></body>`
+  const body = `<body><center class="hello-img"><img width="600" src="/hello.png"></center></body>`
   res.send(`<html>${head}${body}</html>`)
 })
 
