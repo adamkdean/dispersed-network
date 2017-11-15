c0b4c5a744d11cf1d63c38991f465998b41a2456
Adam K Dean <adamkdean@googlemail.com>
2017-11-15 00:02:08 +0000

    feat: add new html to hello world app

--- apps/hello-world/src/server.js
+++ apps/hello-world/src/server.js
@@ -9,8 +9,8 @@ app.use((req, res) => {
   console.log('request for', req.url)
   console.log('req.headers', req.headers)
   const name = req.query && req.query.name || 'no-name'
-  const head = `<head><title>Hello world</title>`
-  const body = `<body><h1>Hello world</h1><h2>Your name is ${name}</h2></body>`
+  const head = `<head><title>Hello world</title><style>body { background: #e1e1e1; }.hello-img { margin-top: 100px; }</head>`
+  const body = `<body><center class="hello-img"><img src="/hello.png"></center></body>`
   res.send(`<html>${head}${body}</html>`)
 })
 
